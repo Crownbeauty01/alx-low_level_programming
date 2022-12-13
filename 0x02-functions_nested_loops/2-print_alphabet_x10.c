ce3c6aae7b5fdae51fd466e6584c0df0dbe53b0c
kareem robiat <krobiat@gmail.com>
2022-12-13 14:16:45 -0800

    me

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,15 +1,18 @@
-/**
- * prints_alphabet -  10 times the alphabet, in lowercase.
- */
-void print_alphabet_x10(void)
-{
-	int count = 0;
-	char letter;
+#include "main.h"
 
-	while (count++ <= 9)
+/**
+ * print_alphabet - prints alphabet
+ *
+ * Return: always 0;
+ *
+ */
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-		_putchar(letter);
-	_putchar('\n');
+		_putchar(i);
 	}
+	_putchar('\n');
 }
